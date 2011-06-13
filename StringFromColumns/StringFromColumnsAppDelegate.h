35a6ed4cf1f8bdab1e85d2c083ed3eb2e94fb8f2
Yuriy Bosov <yuriybosov@Mac-mini-developer.local>
2011-06-13 10:22:54 +0300

    Initial Commit

---
+++ StringFromColumns/StringFromColumnsAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  StringFromColumnsAppDelegate.h
+//  StringFromColumns
+//
+//  Created by Yuriy Bosov on 6/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StringFromColumnsAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
